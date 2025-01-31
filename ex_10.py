1e413dae14943e2b8954aa4d7e8abbe8a636e1fa
Athos <athosbds@gmail.com>
2025-01-31 15:19:22 -0300

    + ex_10

--- ex_10.py
+++ ex_10.py
@@ -1,1 +1,13 @@
 # Python Exercise 61: Redo CHALLENGE 51, reading the first term and the reason of a PA, showing the first 10 terms of the progression using the while structure.
+
+first_term = int(input('Primeiro termo: '))
+reason = int(input('Razão: '))
+
+term = 10
+actually_term = first_term
+count = 1
+while count <= term:
+    print('{}'.format(actually_term), end=' -> ')
+    actually_term += reason
+    count += 1
+print('Fim.')
