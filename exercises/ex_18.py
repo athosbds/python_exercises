8012cd7e761c101341efe9cb2989fd5774a02628
Athos <athosbds@gmail.com>
2025-02-04 15:44:40 -0300

    Trying exercise

---
+++ exercises/ex_18.py
@@ -0,0 +1,11 @@
+#Python Exercise 64: Create a program that reads multiple integers from the keyboard. The program will only stop when the user enters the value 999, which is a stop condition. At the end, show how many numbers were entered and what the sum was between them (ignoring the flag)
+
+number = int(input('Digite um número inteiro'))
+
+flag = True
+
+while flag:
+    if number == 999:
+        
+    else:
+        
