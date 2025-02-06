d6144775ddd122af71657b037e2a871416b69a7e
Athos <athosbds@gmail.com>
2025-02-05 21:22:50 -0300

    ad ex_12

---
+++ while - exercises/ex_12.py
@@ -0,0 +1,23 @@
+#Python Exercise 68: Write a program that plays odd or even with the computer. The game will only stop when the player loses, showing the total number of consecutive wins he has achieved at the end of the game.
+
+import random
+list = list(range(0, 10))
+count = 0
+player = ever_odd = 0
+
+while True:
+    random_number = random.choice(list)
+    player = int(input('Digite um valor: '))
+    odd = str(input('Par ou Ímpar? [I/P]: '))
+    
+    total = player + random_number
+    result = 'P' if total % 2 == 0 else 'I'
+
+    if odd == result:
+        print('Você ganhou')
+        count += 1
+    else:
+        print('GAME OVER! Você perdeu.')
+        break
+print('Venceu {} vezes'.format(count))      
+
