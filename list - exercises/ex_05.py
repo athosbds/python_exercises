0ec3a4c3be4cbab4b09e59b2eea6161e491189c7
Athos <athosbds@gmail.com>
2025-02-19 16:12:58 -0300

    ad ex_05

---
+++ list - exercises/ex_05.py
@@ -0,0 +1,20 @@
+#Exercício Python 082: Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, crie duas listas extras que vão conter apenas os valores pares e os valores ímpares digitados, respectivamente. Ao final, mostre o conteúdo das três listas geradas.
+normal_numbers = []
+even_numbers = []
+odd_numbers = []
+while True:
+    number = int(input('Digite um número: '))
+    normal_numbers.append(number)
+    ask = str(input('Quer continuar? [S/N]: ')).strip().upper()
+    if ask == 'N':
+        break
+    elif ask != 'S':
+        print('Tente Novamente.')
+
+    if number % 2 == 0:
+            even_numbers.append(number)
+    if number % 2 == 1:
+        odd_numbers.append(number)
+print(f'Números Normais: {normal_numbers}')
+print(f'Números Pares: {even_numbers}')
+print(f'Números Ímpares: {odd_numbers}')
