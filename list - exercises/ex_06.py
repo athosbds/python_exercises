49b123e4c66af8dd9be06def633de42eb72e383b
Athos <athosbds@gmail.com>
2025-02-20 19:36:11 -0300

    ad ex_06

---
+++ list - exercises/ex_06.py
@@ -0,0 +1,17 @@
+#Exercício Python 083: Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.
+
+expression = str(input('Digite uma expressão: '))
+pile = []
+for simbol in pile:
+    if expression == '(':
+        expression.append('(')
+    elif expression == ')':
+        if len(pile) > 0:
+            pile.pop()
+        else:
+            pile.append(')')
+            break
+if len(pile) == 0:
+    print('Expressão Válida')
+else:
+    print('Expressão Inválida.')
