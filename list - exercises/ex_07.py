4a44c448342ac27e6925ec1d41dc62af6a4c4c62
Athos <athosbds@gmail.com>
2025-02-25 19:06:19 -0300

    ad ex_07

--- list - exercises/ex_07.py
+++ list - exercises/ex_07.py
@@ -1,14 +1,36 @@
 #Exercício Python 084: Faça um programa que leia nome e peso de várias pessoas, guardando tudo em uma lista. No final, mostre:A) Quantas pessoas foram cadastradas. B) Uma listagem com as pessoas mais pesadas. C) Uma listagem com as pessoas mais leves.
+
 people = []
+data = []
 upper = lower = 0
 while True:
-    data = []
     data.append(str(input('Nome: ')))
     data.append(float(input('Peso: ')))
-    people.append(data)
+    print('-'*20)
+    if len(people) == 0:
+        upper = lower = data[1]
+    else:
+        if data[1] > upper:
+            upper = data[1]
+        if data[1] < lower:
+            lower = data[1]
+    people.append(data[:])
+    data.clear()
     continuos = str(input('Quer Continuar? [S/N]: ')).upper().strip()
+    print('-'*20)
     if continuos == 'N':
         break
-print(f'As pessoas cadastradas foram: {len(people)}')
+print(f'Os dados foram: {people}')
+print(f'\n As pessoas cadastradas foram: {len(people)}')
 
+print(f'O maior peso foi de {upper}kg ', end ='->')
+for person in people:
+    if person[1] == upper:
+        print(f'{person[0]}', end ='')
+
+print(f'\n O menor peso foi de {lower}kg ', end='->')
+for person in people:
+    if person[1] == lower:
+        print(f'{person[0]}', end ='')
+print()
 
