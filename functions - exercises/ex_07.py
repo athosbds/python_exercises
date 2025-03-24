f3c130d966ef4e2b6fc594d68d6e5bb6637d8105
Athos <athosbds@gmail.com>
2025-03-24 19:56:04 -0300

    ad ex_07

--- functions - exercises/ex_07.py
+++ functions - exercises/ex_07.py
@@ -1,1 +1,16 @@
 #Exercício Python 102: Crie um programa que tenha uma função fatorial() que receba dois parâmetros: o primeiro que indique o número a calcular e outro chamado show, que será um valor lógico (opcional) indicando se será mostrado ou não na tela o processo de cálculo do fatorial.
+
+def fat(number, show=False):
+    result = 1
+    for x in range(number, 0, -1):
+        if show:
+            print(x, end='')
+            if x>1:
+                print(' x ', end='')
+            else:
+                print(' = ', end='')
+        result *= x
+    return result
+    
+print(fat(5, True))
+print(fat(5, False))
