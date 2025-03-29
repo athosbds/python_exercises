73bf2b0d29ef03b50dec26216db4617ad682d886
Athos <athosbds@gmail.com>
2025-03-29 12:26:16 -0300

    ad ex_11

---
+++ functions - exercises/ex_11.py
@@ -0,0 +1,13 @@
+#Exercício Python 106: Faça um mini-sistema que utilize o Interactive Help do Python. O usuário vai digitar o comando e o manual vai aparecer. Quando o usuário digitar a palavra ‘FIM’, o programa se encerrará. Importante: use cores.
+def interatictive_help():
+    while True:
+        print('\033[42m Sistema de Ajuda PyHELP\033[m')
+        helped = str(input('Função ou Biblioteca: '))
+        if helped != 'fim':
+            print(f'\033[44m Acessando o manual do comando ´{helped}´\033[m')
+            help(helped)
+        else:
+            print('\033[41m      Até logo     \033[m')
+            break
+interatictive_help()
+
