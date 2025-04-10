92faac9ba8a245d75c6a00b0181f13e5262db360
Athos <athosbds@gmail.com>
2025-04-10 18:52:05 -0300

    ad ex_03.py finally

--- error handling - exercises/ex_03.py/utillity.py
+++ error handling - exercises/ex_03.py/utillity.py
@@ -4,16 +4,36 @@ def menu():
         print("""
             1 - Ver pessoas Cadastradas
             2 - Cadastrar Nova Pessoa
-            3 - Sair do Programa""")
+            3 - Sair do Programa
+            4 - Limpar Cadastro""")
         
         try:
             choice = int(input('\nSua Escolha: '))
             if choice == 1:
                 print(f'{"OPÇÃO 1":>40}')
+                try:
+                    with open('people.txt', 'r') as arq:
+                        content = arq.read()
+                    if content.strip() == '':
+                        print('Nenhuma Pessoa Cadastrada ainda.')
+                    else:
+                        print(content)
+                except FileNotFoundError:
+                    print('Não Encontrado')
             elif choice == 2:
-                print(f'{"OPÇÃO":>40}')
+                name = input('Nome da Pessoa: ')
+                print(f'{"OPÇÃO 2":>40}')
+                with open('people.txt', 'a') as arq:
+                    arq.write(name + '\n')
+                print(f'{name} cadastrado(a) com sucesso!')
             elif choice == 3:
-                print('Saindo do programa...')
+                print('SAINDO DO PROGRAMA...')
+                break
+            elif choice == 4:
+                print(f'{"OPÇÃO 4":>40}')
+                with open('people.txt', 'w') as arq:
+                    arq.write('')
+                print('LIMPO COM SUCESSO.')
             else:
                 print('Opção Inválida.')
         except ValueError:
